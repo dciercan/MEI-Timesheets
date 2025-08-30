415dd49bad6119209b2196805d53f1fbfda39bf0
David <dciercan@gmail.com>
2025-08-30 23:30:09 +0000

    Spark Admins dont need the Timesheet Entry menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ import {
 
 
 const defaultLinks = [
-  { href: '/timesheet', label: 'Timesheet Entry', icon: User, roles: ['Crew Supervisor', 'Admin', 'Subcontractor Admin', 'MEI Supervisor'] },
+  { href: '/timesheet', label: 'Timesheet Entry', icon: User, roles: ['Crew Supervisor', 'Subcontractor Admin', 'MEI Supervisor'] },
   { href: '/timesheet/my-submissions', label: 'My Submissions', icon: FileText, roles: ['Crew Supervisor', 'MEI Supervisor'] },
 ];
 
